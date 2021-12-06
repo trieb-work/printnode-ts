bb588fa1d3b12e96b789d76fa616f3f3d6f23007
Jannik Zinkl <jannik@trieb.work>
2021-12-06 19:40:23 +0100

    fix: some stupid settings

--- test/printnode-ts.test.ts
+++ test/printnode-ts.test.ts
@@ -1,14 +1,14 @@
-import DummyClass from "../src/printnode-ts"
+import Printnode from '../src/printnode-ts'
 
 /**
  * Dummy test
  */
-describe("Dummy test", () => {
-  it("works if true is truthy", () => {
+describe('Dummy test', () => {
+  it('works if true is truthy', () => {
     expect(true).toBeTruthy()
   })
 
-  it("DummyClass is instantiable", () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  it('Printnode is instantiable', () => {
+    expect(new Printnode({ apiKey: '45646' })).toBeInstanceOf(Printnode)
   })
 })
